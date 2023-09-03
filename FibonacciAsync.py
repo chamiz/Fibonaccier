96efc5a1245aa389e19f4f55b8e5ab3fb0c8ae36
chamiz <chamila.ambahera@gmail.com>
2023-09-03 23:57:50 +0200

    Added method comments

--- FibonacciAsync.py
+++ FibonacciAsync.py
@@ -1,15 +1,18 @@
+""""Calculates Fibonacci sequence Asynchronously"""
 import asyncio
 import random
 
 
-async def fibonacci_of(n):
-    if n in {0, 1}:  # Base case
-        return n
+async def fibonacci_of(input_int):
+    """"Calculates Fibonacci sequence"""
+    if input_int in {0, 1}:  # Base case
+        return input_int
     await asyncio.sleep(random.random())
-    return await fibonacci_of(n - 1) + await fibonacci_of(n - 2)  # Recursive case
+    return await fibonacci_of(input_int - 1) + await fibonacci_of(input_int - 2)  # Recursive case
 
 
 async def main():
+    """"Execute methods concurrently"""
     tasks = []
     for i in range(2):
         # User input from console
@@ -20,11 +23,12 @@ async def main():
 
     # Return the first completed task
     for task in done:
-        print("First result --> " + task.result().__str__())
+        print("First result --> " + str(task.result()))
 
     # Return the results of second task
     for task in pending:
         await asyncio.wait(pending)
-        print("Second result --> " + task.result().__str__())
+        print("Second result --> " + str(task.result()))
+
 
 asyncio.run(main())
